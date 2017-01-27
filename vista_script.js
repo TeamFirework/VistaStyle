7853cc1747eb28d4c9ff728b7ac51db981cb8392
bleonard252 <bleonard252@rocketmail.com>
2017-01-27 15:26:31 -0500

    Update vista_script.js

--- vista_script.js
+++ vista_script.js
@@ -1,1 +1,5 @@
 
+/* Must make script light, include intermissiions.
+Seperate scripts if neccessary. */
+
+alert("Hooray, you installed the Vista script successfully! It's not fully functioning right now, so please have patience.");
