a7540b2283f2f0fd63d37148ed9d64a9900c4d12
bleonard252 <bleonard252@rocketmail.com>
2017-01-27 21:48:53 -0500

    Update vista_script.js

--- vista_script.js
+++ vista_script.js
@@ -3,3 +3,6 @@
 Seperate scripts if neccessary. */
 
 alert("Hooray, you installed the Vista script successfully! It's not fully functioning right now, so please have patience.");
+
+// The ID for the tag is "vistajs_script" and after it is <vista id="vistajs_script_post" />
+document.getElementById('vistajs_script_post').write("<link css
