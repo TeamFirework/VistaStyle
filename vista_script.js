4ce57e92f84fe006e5820aed63bf484dca180208
bleonard252 <bleonard252@rocketmail.com>
2017-01-27 22:26:57 -0500

    Update vista_script.js

--- vista_script.js
+++ vista_script.js
@@ -1,8 +1,11 @@
 
-/* Must make script light, include intermissiions.
+/* Must make script light, include intermissions.
 Seperate scripts if neccessary. */
 
 alert("Hooray, you installed the Vista script successfully! It's not fully functioning right now, so please have patience.");
 
 // The ID for the tag is "vistajs_script" and after it is <vista id="vistajs_script_post" />
-document.getElementById('vistajs_script_post').write("<link css
+document.getElementById('vistajs_script_post').write('<link rel="stylesheet" href="vista.b252.gq/vista.css">\n<vista id="vistajs_script_post" />');
+
+console.log("Vista Style script loaded successfully."); console.log("Vista Stylesheet installed successfully.");
+console.log("Vista Style script finished loading.");
